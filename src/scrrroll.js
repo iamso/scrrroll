7bb5c16134fd6d4301b3ea6c64820bd06747f738
iamso <so@dev.so>
2019-12-12 15:40:36 +0100

    prevent endless loop when document height changes during scroll down

--- src/scrrroll.js
+++ src/scrrroll.js
@@ -81,23 +81,24 @@ export default class Scrrroll {
       const windowHeight = this.winHeight;
       const destinationOffset = typeof destination === 'number' ? destination : Math.ceil(destination.getBoundingClientRect().top + window.pageYOffset) - defaults.offset;
       const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+      const down = start < destinationOffsetToScroll;
 
       if (!/^f/.test(typeof easing)) {
         easing = defaults.easing;
       }
 
-      function scroll() {
+      const scroll = () => {
         const now = 'now' in window.performance ? performance.now() : Date.now();
         const time = Math.min(1, ((now - startTime) / duration));
         const timeFunction = easing(time);
         window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
 
-        if (Math.abs(window.pageYOffset - destinationOffsetToScroll) < 1) {
+        if (Math.abs(window.pageYOffset - destinationOffsetToScroll) < 1 || (down && window.pageYOffset >= (this.docHeight - this.winHeight))) {
           resolve();
           return;
         }
         frame = requestAnimationFrame(scroll);
-      }
+      };
 
       cancelAnimationFrame(frame);
       scroll();
